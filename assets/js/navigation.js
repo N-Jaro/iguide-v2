2f64d044ccd02213e4ace1986cbfffc4d411ec1a
Nattapon Jaroenchai <b_nj18@live.com>
2023-02-16 13:29:07 -0600

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -62,7 +62,7 @@ $(function () {
         var motto = mottos[slideNumber++];
         $(motto).click();
         if(slideNumber >= mottos.length) i = 0;
-    }, 100000); 
+    }, 5000); 
 
     // Click function
     $('#tabs-nav span').on('click', function(){

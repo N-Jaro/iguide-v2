7cb71402fb29bee59504e28ae1c4dd2b2aa6fea1
Furqan Baig <fbaig@illinois.edu>
2023-02-27 14:16:09 -0600

    updated platform launch button label + added button to launch catalog

--- page-platform.php
+++ page-platform.php
@@ -31,7 +31,9 @@
                                 <div class="row align-items-center">
                                     <div class="col-12 text-center">
                                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/IGUIDE_logoH_ColorMulti.png" style="width: 70%;" class="mb-5 img-fluid"> <br>
-                                        <a href="<?php echo home_url('/discovery');?>" class="btn btn-warning shadow-sm">Launch I-GUIDE Platform</a>
+                                        <a href="<?php echo home_url('/discovery');?>" class="btn btn-warning shadow-sm">Launch I-GUIDE Featured Notebooks</a>
+                                        <br>
+					<a href="https://iguide.cuahsi.io/discover" class="btn btn-warning shadow-sm">Launch I-GUIDE Data Catalog</a>
                                         <br>
                                         <a style="font-size:11px;text-decoration:none;color:#a1a1a1;" class="text-center" href="<?php echo home_url('/iguide-data-and-computation-resources');?>">More I-GUIDE Resources</a>
                                     </div>

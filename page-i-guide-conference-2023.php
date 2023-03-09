e55400b57a4276503dc890475f4e0bf9da677298
Nattapon Jaroenchai <b_nj18@live.com>
2023-03-09 11:01:27 -0600

    Update page-i-guide-conference-2023.php

--- page-i-guide-conference-2023.php
+++ page-i-guide-conference-2023.php
@@ -5,19 +5,15 @@ if(is_child_of(get_page_ID_by_slug('platform'))) { // check if it is platform
 else {
     get_header();
 ?>
+<style>
+    img{ border-radius: 0 !important;}
+</style>
     <div class="page-title tw-w-full tw-block  tw-relative tw--mt-[200px]" >
         <div class="tw-container tw-mx-auto tw-px-4 tw-h-min-[200px]">
             <div class="tw-border-l-8 tw-pl-3 tw-border-ig-orange tw-text-white tw-font-semibold tw-text-2xl">
                 <h1><?php the_title();?></h1>
             </div>
-            <!-- <p class="tw-text-light tw-text-sm tw-text-white tw-pt-2 tw-pl-5"><?php my_post_time_ago(); ?></p> -->
         </div>
-        <!-- <div class="custom-shape-divider-bottom-1675786129">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
-                preserveAspectRatio="none">
-                <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
-            </svg>
-        </div> -->
     </div>
 
     <div class="page-content tw-w-full tw-flex tw-relative tw-mt-5">
